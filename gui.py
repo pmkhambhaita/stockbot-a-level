51caad0fae2b789dc9faa94fb03f9f0fdb980c96
pmkhambhaita <pmkhambhaita@outlook.com>
2025-04-15 14:36:23 +0100

    modified tkinter manager to use 'grid' instead of 'pack'

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ class PathfinderGUI:
         self.root = root
         self.root.title("StockBot")
         self.root.geometry("600x400")
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
 
 def main():
     root = tk.Tk()
